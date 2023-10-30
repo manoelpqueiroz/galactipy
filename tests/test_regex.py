4434e0b24feed8e089823b43138f74544f76c127
Manoel Pereira de Queiroz <mpq.dev@pm.me>
2023-10-30 20:36:22 -0300

    :white_check_mark: Add repo name unit tests
    Created unit tests to check `validate_repo_name`, which include:
    
    - Tests for valid names;
    - Tests for invalid names under the regex rules;
    - Tests for invalid names combining two of more infringements to the
      regex.
    
    Changelog: testing

---
+++ tests/test_regex.py
@@ -0,0 +1,140 @@
+import pytest
+
+from hooks.pre_gen_project import RESERVED_PROJECTS, validate_repo_name
+
+
+@pytest.mark.parametrize(
+    "valid_slug",
+    [
+        "9zUZCqQAOP1NjAgU",
+        "o3fYbfg2kMjJjQd3DHKPnbp",
+        "vBmu8T8MGL",
+        "Odcp6pqK2Y4I.PKIZ0WfbZj",
+        "rQV.SgJhbsy.n7GhitwpnM_rN",
+        "9SwPJgigH",
+        "Kn7E2vsji1",
+        "BgaoXUu.gJxBDpLV",
+        "AjgB",
+        "WCdj7Ii6sZUtCxIeBGh1",
+        "nVB2",
+        "B7cOcPkOYo9nF0-jlaLqYoK3",
+        "f07gwCbWZK-prBg",
+        "WxCKHO-TpiyP",
+        "4kc_xSi",
+        "HnH1-K5Cj2GtdG3wnNM6V",
+        "JUr.xHPsKW5pc_MhZiSEp",
+        "65cY_A71B3GgaYrw68a",
+        "MhD-k5p",
+        "MGA0xNXL8PHIOEnD",
+        "BsX-ZRfSIr-c0yDl",
+        "hs7caUTKZNo",
+        "BS7l1HUDL3R7tpqqsYw",
+        "feC5BkNT8.SvRDpI.0",
+        "ZtwAd3cwEF",
+        "mpf6",
+        "lly31685PEyWLngr",
+        "cltK.gti",
+        "1HRIiu0YhWfkDy37Fj9Xdmv.gt",
+        "FBa_vC07MB2Ib.gi",
+        "JdUzsPiRbNs9tN02kXgJglQ.it",
+        "li.fF_git",
+        "gziUigB-git",
+        "dK0Ry9KAW934afvdmyY.atmo",
+        "bP4xRFjShWp1kFn.tom",
+        "Kf0h2hIn-zG0JlrJ7JZT.aom",
+        "s_5mA_mlQZfSYwyT0O5h3GN.atm",
+        "jaXXN-toaAboZ2szbOW-mTQ.ato",
+        "8DcrvdJB4C3O30g1xlHTbypj_atom",
+        "ChK4ASrqjt_0G-atom",
+    ],
+)
+def test_valid_repo_names(valid_slug):
+
+    assert validate_repo_name(valid_slug, RESERVED_PROJECTS) is None
+
+
+@pytest.mark.parametrize(
+    "invalid_slug",
+    [
+        r"\-",
+        "badges",
+        "blame",
+        "blob",
+        "builds",
+        "commits",
+        "create",
+        "create_dir",
+        "edit",
+        "environments/folders",
+        "files",
+        "find_file",
+        "gitlab-lfs/objects",
+        "info/lfs/objects",
+        "new",
+        "preview",
+        "raw",
+        "refs",
+        "tree",
+        "update",
+        "wikis",
+        ".XGiovqokdpk30Sc+S",
+        "-dtPTEHNDy1o",
+        "_9LMkzhMIV6FwZ",
+        "Prc9OVWpQJOn+",
+        "JEWF2BDkg.",
+        "MeCvP0U1wxRxjLFgfSZ5-",
+        "JEUcDGf_",
+        "aaJslw..vJ0wgGtJIF",
+        "ywt0--zdgE",
+        "1xmGB3qn__71TxSnXZKywl3TK",
+        "fgks-_EOgiGHjQ5",
+        "4WLLRG-4h8_-B1F_3WEc",
+        "lZ._V.sGfZz5qCiR2",
+        "d1_.NU9BHcCW1",
+        "3a-.9w",
+        "O1rEaP2.-sD",
+        "bPcH@ARcZ",
+        "e+Qj",
+        "A7dSDs*8Ug",
+        "4UnB#Se4dJ4",
+        "OY_O3KjBLZ;KJ-hpvjox/Dveg2",
+        "t0bmh!sj55",
+        "tGcyDjUQoZ-ecMuyUCiFVd.git",
+        "cFLtG.4Q0lty8vNjMew1kef.atom",
+    ],
+)
+def test_invalid_repo_names(invalid_slug):
+
+    with pytest.raises(ValueError):
+        validate_repo_name(invalid_slug, RESERVED_PROJECTS)
+
+
+@pytest.mark.parametrize(
+    "invalid_slug",
+    [
+        "+fit9YR",
+        "/Xcpjw8jkjHZH2X",
+        "@PigZnNND",
+        "NWQX9PL5mF+",
+        "xKc3YrKCVNOahZeGvIQH31ye!",
+        "kci2h2GC0P2MzlCjUyzcx#",
+        ".gczpb.",
+        "-UtE6tqfvAqC8iltmgIu-",
+        "_JJeUvmIGQy_",
+        ".SOXJ6_",
+        "-izVxmqfqc6XsTqDnQ5Cy6uk.",
+        "2MeqULP...wK5vbUZovmkeL6mL",
+        "ctfwycYR6AUs---NDEl",
+        "WsA___YVYYdPb1hf57Z",
+        "umVbUkERv++nPYbWJXTD7",
+        "q7JB@@v8yZpYanxDSkHzhXy",
+        "1OXPt**Y",
+        "1AM7Kbp63OEU##mEwYHRTKtk",
+        "gxiho8L..git",
+        "3Z1ORQzFftL9xqa-.atom",
+    ],
+)
+def test_doubly_invalid_repo_names(invalid_slug):
+
+    with pytest.raises(ValueError):
+        validate_repo_name(invalid_slug, RESERVED_PROJECTS)
