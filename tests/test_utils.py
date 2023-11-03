f2939ac007494697c17b9f47d4bf24514f250236
Manoel Pereira de Queiroz <mpq.dev@pm.me>
2023-11-03 18:14:32 -0300

    :white_check_mark: Add class test for file/directory operations
    Created unit tests for validating the `rmdir` function using pytest's
    `tmp_path` built-in fixture. The tests created here run simple checks on
    the function for:
    
    - Single file;
    - Empty directory;
    - Directory with files.
    
    Changelog: testing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 import pytest
 
+from hooks.post_gen_project import rmdir
 from hooks.pre_gen_project import validate_line_length
 
 
@@ -11,3 +12,40 @@ def test_validate_line_length():
 
     with pytest.raises(ValueError):
         validate_line_length(-10)
+
+
+class TestRmDir:
+    def test_remove_single_file(self, tmp_path):
+        file_path = tmp_path / "test_file.txt"
+        file_path.touch()
+
+        rmdir(file_path)
+
+        assert not file_path.exists()
+
+    def test_remove_empty_directory(self, tmp_path):
+        empty_dir = tmp_path / "empty_dir"
+        empty_dir.mkdir()
+
+        rmdir(empty_dir)
+
+        assert not empty_dir.exists()
+
+    def test_remove_directory_with_files(self, tmp_path):
+        directory = tmp_path / "test_directory"
+        directory.mkdir()
+
+        file_alpha = directory / "alpha.txt"
+        file_beta = directory / "beta.txt"
+        file_gaga = directory / "gaga.txt"
+
+        file_alpha.touch()
+        file_beta.touch()
+        file_gaga.touch()
+
+        rmdir(directory)
+
+        assert not directory.exists()
+        assert not file_alpha.exists()
+        assert not file_alpha.exists()
+        assert not file_gaga.exists()
