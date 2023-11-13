ee39ce7d394088a9f15af991b976ef64f28d887d
Manoel Pereira de Queiroz <mpq.dev@pm.me>
2023-11-13 10:51:58 -0300

    :heavy_plus_sign: Add pytest-benchmark to template dependencies
    Added the pytest-benchmark plugin to template dependencies to showcase
    how extensive one can get with testing. Included the library's minimal
    example adapted to the `hello` function's context in a new text.
    
    Changelog: dependencies

--- {{ cookiecutter.repo_name }}/tests/test_hello.py
+++ {{ cookiecutter.repo_name }}/tests/test_hello.py
@@ -2,9 +2,20 @@
 # UPDATEME by removing this file once the `hello` function is no longer needed for your
 # project and `example.py` is also removed
 
+import time
+
 import pytest
 
 from {{ cookiecutter.package_name }}.example import hello
+
+
+def something(duration=0.000001):
+    """Generic function to showcase benchmarking.
+
+    """
+    time.sleep(duration)
+
+    return "Hello Aaron"
 
 
 @pytest.mark.parametrize(
@@ -26,3 +37,18 @@ def test_hello(name, expected):
 
     """
     assert hello(name) == expected
+
+
+def test_my_stuff(benchmark):
+    """Example test with benchmark.
+
+    """
+    # Benchmark the something function with pytest-benchmark
+    result = benchmark(something)
+
+    # Extra code, to verify that the run completed correctly
+    expected = hello("Aaron")
+
+    # Sometimes you may want to check the result against your benchmark, writing faster
+    # functions are no good if they return incorrect results
+    assert result == expected
