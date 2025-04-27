8444f8f2162b3c33dc1d09f6497178333b1797d2
Manoel Pereira de Queiroz <mpq.dev@pm.me>
2025-04-27 19:32:41 -0300

    :sparkles: Implement BDD for CLI app
    To further enable BDD features for the template, the tests for the root
    command were expanded, including an additional test asserting failed
    terminations of the program if run with any command and replicating
    these same tests using a feature to define expected behaviour.
    
    The `pyproject.toml` file was updated to exclude the `__main__.py` file,
    as now its only function is to allow program execution with `python -m`,
    while the failure threshold for coverage has been raised to 90% after
    changes removing the example code
    
    Changelog: feature

--- {{ cookiecutter.repo_name }}/tests/cli/test_root_command.py
+++ {{ cookiecutter.repo_name }}/tests/cli/test_root_command.py
@@ -1,17 +1,68 @@
 # UPDATEME with additional tests for each command, see documentation at:
 # https://typer.tiangolo.com/tutorial/testing/
 from typer.testing import CliRunner
-from rich.text import Text
 
 from {{ cookiecutter.package_name }}.cli.root_command import app
 
+{% if cookiecutter.use_bdd -%}
+from pytest_bdd import scenario, when, then, parsers
+{% else -%}
+import pytest
+{%- endif %}
+
 runner = CliRunner()
 
-def test_version():
-    result = runner.invoke(app, ["--version"])
-    assertion_string = Text.from_markup(
-        ":package: {{ cookiecutter.project_name}} 0.0.0\n"
-    )
 
+{% if cookiecutter.use_bdd -%}
+@scenario("root_command.feature", "Invoke with version argument")
+def test_app_with_version_arg():
+    pass
+
+
+@when("the program is called with the `--version` argument", target_fixture="app_run")
+def invoke_version_arg():
+    return runner.invoke(app, args=["--version"])
+
+
+@then("the program's version is displayed")
+def version_display(app_run, version_string):
+    assert app_run.stdout == version_string
+
+
+@then("the program is terminated without errors")
+def successful_termination(app_run):
+    assert app_run.exit_code == 0
+
+
+# TODO replace scenario when new commands are added to the CLI
+@scenario("root_command.feature", "Invoke any command")
+def test_app_with_any_command():
+    pass
+
+
+@when(
+    parsers.parse("the program is called with the {invalid} command"),
+    target_fixture="app_run",
+)
+def invoke_invalid_command(invalid):
+    return runner.invoke(app, args=[invalid])
+
+
+@then("the program is terminated with status 2")
+def return_status_2(app_run):
+    assert app_run.exit_code == 2
+{%- else -%}
+def test_app_with_version_arg(version_string):
+    result = runner.invoke(app, args=["--version"])
+
+    assert result.stdout == version_string
     assert result.exit_code == 0
-    assert result.stdout == assertion_string.plain
+
+
+# TODO replace this test when new commands are added to the CLI
+@pytest.mark.parametrize("invalid", ["any", "some", "testing", "help", "run"])
+def test_app_with_any_command(invalid):
+    result = runner.invoke(app, args=[invalid])
+
+    assert result.exit_code == 2
+{%- endif %}
