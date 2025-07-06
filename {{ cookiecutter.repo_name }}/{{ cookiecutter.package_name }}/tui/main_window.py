d0a798a6354dfc8d594f7de3d8173d78a392a8ed
Manoel Pereira de Queiroz <mpq.dev@pm.me>
2025-07-06 17:03:49 -0300

    :sparkles: Create demo TUI application
    Provided a simple application using Textual to be placed in the `tui`
    directory. This application uses `art` to generate a stylish startup
    screen with an initial configuration so users can build up from there.
    
    While this implementation breaks the design principle of avoiding extra
    content that will not carry to user projects, it does so with minimal
    intervention: users just need to remove two lines of code and the `.tcss`
    file to have a clean slate for their projects.
    
    Closes #211
    
    Changelog: feature

---
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/tui/main_window.py
@@ -0,0 +1,31 @@
+# UPDATEME With additional components in `tui/components/`
+# See Textual documentation at:
+# https://textual.textualize.io/tutorial/
+from pathlib import Path
+
+from art import text2art
+
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Label
+
+
+class TerminalApp(App):
+    """Textual app to serve as the {{ cookiecutter.project_name }} interface."""
+
+    CSS_PATH = Path(__file__).parent / "css" / "demo.tcss"
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        # UPDATEME by replacing with your own widgets
+        yield Label(text2art("{{ cookiecutter.project_name }}", "tarty1"), classes="title")
+        yield Label("[i][b]{{ cookiecutter.project_description }}[/]", classes="description")
+        yield Footer()
+
+    def on_mount(self) -> None:
+        """Execute instructions when launching the interface."""
+        self.theme = "nord"
+
+
+if __name__ == "__main__":
+    app = TerminalApp()
+    app.run()
