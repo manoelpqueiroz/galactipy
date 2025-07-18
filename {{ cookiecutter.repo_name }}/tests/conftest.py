eb293fcca2d8f067419b2f3896f407d73db65cc2
Manoel Pereira de Queiroz <mpq.dev@pm.me>
2025-07-18 17:25:27 -0300

    :white_check_mark: Rework BDD unit test for TUI
    Made adjustments to the BDD implementation of the TUI unit test by
    incorporating the workaround for asynchronous steps found in the
    `pytest-bdd` issue tracker.
    
    Also added two packages inside the template's `tests` directory,
    containing the workaround decorator and a helper class to incorporate
    all TUI application elements for testing in a single place, thus
    ensuring separation of concerns.
    
    The template `pyproject.toml` has been updated with an additional Pytest
    option, removing the need to specify async marks.
    
    Closes #273
    
    Changelog: template-config

--- {{ cookiecutter.repo_name }}/tests/conftest.py
+++ {{ cookiecutter.repo_name }}/tests/conftest.py
@@ -1,3 +1,7 @@
+{%- if cookiecutter.use_bdd %}
+import asyncio
+
+{%- endif %}
 from rich.text import Text
 
 import pytest
@@ -6,3 +10,13 @@ import pytest
 @pytest.fixture
 def version_string():
     return Text.from_markup(":package:{{ cookiecutter.project_name}} 0.0.0\n").plain
+
+
+{% if cookiecutter.use_bdd -%}
+@pytest.fixture(scope="function")
+def event_loop():
+    """Create an instance of the default event loop for the test session."""
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
+{%- endif %}
--- {{ cookiecutter.repo_name }}/tests/tui/test_main_window.py
+++ {{ cookiecutter.repo_name }}/tests/tui/test_main_window.py
@@ -1,40 +1,38 @@
 # UPDATEME with additional tests for different interactions, see documentation at:
 # https://textual.textualize.io/guide/testing/
-from {{ cookiecutter.package_name }}.tui.main_window import TerminalApp
-
 {% if cookiecutter.use_bdd -%}
-import pytest
-from pytest_bdd import scenario, when, then, parsers
+from pytest_bdd import scenario, given, when, then
+
+from tests.utils.pytest_bdd_async import async_step
+from tests.helpers.tui_helpers import AppInterface
+
 {% else -%}
-import pytest
+from {{ cookiecutter.package_name }}.tui.main_window import TerminalApp
 {%- endif %}
 
 
 {% if cookiecutter.use_bdd -%}
 @scenario("main_window.feature", "Quit the interface")
-@pytest.mark.asyncio
-async def test_app_exit():
+def test_app_exit():
     pass
 
-@given("the interface is running")
-@pytest.mark.asyncio
-def running_interface(target_fixture="interface_run"):
-    interface = TerminalApp()
 
-    yield interface.run_test()
+@given("the interface is running", target_fixture="interface_run")
+def running_interface():
+    return AppInterface()
+
 
 @when("the user presses `Ctrl+Q`")
-@pytest.mark.asyncio
-def press_ctrl_q(interface_run):
-    async with interface_run as pilot:
+@async_step
+async def press_ctrl_q(interface_run):
+    async with interface_run.pilot as pilot:
         await pilot.press("ctrl+q")
 
+
 @then("the program is terminated without errors")
-@pytest.mark.asyncio
 def successful_termination(interface_run):
     assert interface_run.return_code == 0
 {%- else -%}
-@pytest.mark.asyncio
 async def test_app_exit():
     interface = TerminalApp()
 
