f005dd9abc8b98a47dca4164d99e51472c02f512
Manoel Pereira de Queiroz <mpq.dev@pm.me>
2025-09-19 20:55:50 -0300

    :wrench: Rework project tasks
    Reworked the entire tasks file for the project. Streamlined expected
    errors using Invoke's `Exit` exception, removed unnecessary tasks
    relating to Poetry as developers are expected to at least have it
    installed when cloning the repo and leveraged Invoke's context to
    provide the most accurate virtualenv path at each task runtime, removing
    the need for the `utils` package altogether.
    
    Additionally, most of the tasks were renamed to become more simple to be
    called as commands.
    
    Closes #221, #222, #224
    
    Changelog: project-config

--- tasks.py
+++ tasks.py
@@ -1,26 +1,30 @@
-from utils.resolution import UNIX_OS as PTY
-from utils.resolution import get_commands, get_venv_bin
+import os
+from pathlib import Path
+from shutil import which
+
+INSTALLATION_ERROR_MSG = (
+    "{name} installation not found in the system. Install it through your "
+    "distribution's package manager if available; otherwise install it via pipx with "
+    "`pipx install {package}`. If you have {name} already installed, your virtual "
+    "environment most likely is having trouble choosing the executable to run"
+)
 
 try:
-    from invoke import Context, task
+    from invoke import Context, call, task
+    from invoke.exceptions import Exit
+
 except ModuleNotFoundError as e:
-    msg = (
-        "Invoke was not found in your system. Install it through your distribution's "
-        "package manager if available, otherwise install it preferrably via pipx with "
-        "`pipx install invoke` or through `pip install invoke --user`. If you have "
-        "Invoke already installed, your virtual environment most likely is having "
-        "trouble choosing the executable to run."
-    )
-    raise ModuleNotFoundError(msg) from e
+    raise ModuleNotFoundError(
+        INSTALLATION_ERROR_MSG.format(name="Invoke", package="invoke")
+    ) from e
 
-PACKAGE_NAME = "galactipy"
+IS_UNIX_OS = os.name != "nt"
+BIN_DIR = "bin" if IS_UNIX_OS else "Scripts"
 
-# Default paths
-VENV_BIN = get_venv_bin(PACKAGE_NAME)
-PYTHON_PATH, POETRY_PATH = get_commands(VENV_BIN)
+PROJECT_NAME = "galactipy"
 
 # Reusable command templates
-if PTY:
+if IS_UNIX_OS:
     FILE_REMOVER = 'find . | grep -E "{}" | xargs rm -rf'
 
 else:
@@ -31,174 +35,195 @@ else:
     )
 
 
-class PoetryPluginError(Exception):
-    """Raised when a Poetry plugin is unable to be installed."""
+def get_poetry_command() -> Path:
+    """Retrieve the executable path for Poetry.
 
-    pass
+    Returns
+    -------
+    Path
+        Path to the Poetry executable. If no path is found, will use the
+        system's default paths for a Poetry installation.
+    """
+    poetry_command = which("poetry")
+
+    if poetry_command is not None:
+        poetry_path = Path(poetry_command).resolve()
+
+    else:
+        raise Exit(INSTALLATION_ERROR_MSG.format(name="Poetry", package="poetry"))
+
+    return poetry_path
+
+
+# Foundational tasks, used as the basis for running other tasks
+@task
+def venv(c: Context, hide: bool = False) -> None:
+    """Add Poetry executable and project environment binaries to context."""
+    poetry_path = get_poetry_command()
+
+    result = c.run(f"{poetry_path} env info --path", hide=hide, warn=True)
+
+    if result.failed:
+        msg = f"unable to fetch Poetry virtual environment for {PROJECT_NAME}"
+        raise Exit(msg)
+
+    c.venv_bin_path = Path(result.stdout.strip()).resolve() / BIN_DIR
+
+
+@task(call(venv, hide=True))
+def mypy(
+    c: Context, install_types: bool = False, non_interactive: bool = False
+) -> None:
+    """Run type checks with `mypy` and `pyproject.toml` configuration."""
+    install_flag = "--install-types" if install_types else ""
+    non_interactive_flag = "--non-interactive" if non_interactive else ""
+
+    c.run(
+        f"{c.venv_bin_path}/mypy --config-file pyproject.toml {install_flag} "
+        f"{non_interactive_flag}",
+        pty=IS_UNIX_OS,
+    )
+
+
+@task(call(venv, hide=True), aliases=["pre-commit-install", "install-hooks"])
+def hooks(c: Context) -> None:
+    """Install pre-commit hooks."""
+    c.run(f"{c.venv_bin_path}/pre-commit install", pty=IS_UNIX_OS)
 
 
 # Poetry tasks
-@task
-def poetry_download(c: Context) -> None:
-    """Install Poetry as a standalone package via cURL.
-
-    As per Poetry documentation, it should be either installed through a Python
-    script which creates a temporary venv and installs Poetry in a
-    platform-specific path or via pipx.
-
-    More information can be obtained via https://python-poetry.org/docs/#installation
-    and the script instructions in https://install.python-poetry.org/.
-
-    """
-    c.run(f"curl -sSL https://install.python-poetry.org | {PYTHON_PATH} -", pty=PTY)
-
-
-@task
-def poetry_remove(c: Context) -> None:
-    """Remove Poetry using the standalone installation script."""
-    c.run(
-        f"curl -sSL https://install.python-poetry.org | {PYTHON_PATH} - --uninstall",
-        pty=PTY,
-    )
-
-
-@task
-def poetry_check(c: Context) -> None:
+@task(aliases=["poetry-check"])
+def pyproject(c: Context) -> None:
     """Check `pyproject.toml` configuration."""
-    c.run(f"{POETRY_PATH} check", pty=PTY)
+    poetry_path = get_poetry_command()
+
+    c.run(f"{poetry_path} check", pty=IS_UNIX_OS)
 
 
-@task
-def update_dev_deps(c: Context) -> None:
-    """Update dev dependencies to their latest configuration."""
-    c.run(f"{POETRY_PATH} up --only=dev --latest", pty=PTY)
+@task(aliases=["update-deps"])
+def update(c: Context, latest: bool = False) -> None:
+    """Update dependencies to their latest configuration."""
+    poetry_path = get_poetry_command()
+    flag = "--latest" if latest else ""
+
+    c.run(f"{poetry_path} up {flag}", pty=IS_UNIX_OS)
 
 
 # Installation tasks
-@task
+@task(post=[hooks, call(mypy, install_types=True, non_interactive=True)])
 def install(c: Context, ignore_pty: bool = False) -> None:
-    """Install dependencies specified in `pyproject.toml` and run mypy."""
-    local_pty = False if ignore_pty else PTY
+    """Install dependencies specified in `pyproject.toml` and run MyPy."""
+    poetry_path = get_poetry_command()
+    local_pty = False if ignore_pty else IS_UNIX_OS
 
-    c.run(f"{POETRY_PATH} lock -n", pty=local_pty)
-    c.run(f"{POETRY_PATH} install -n", pty=local_pty)
-
-    local_venv_bin = get_venv_bin(PACKAGE_NAME)
-    c.run(
-        f"{local_venv_bin}/mypy --config-file pyproject.toml --install-types "
-        "--non-interactive",
-        pty=local_pty,
-    )
-
-
-@task
-def pre_commit_install(c: Context) -> None:
-    """Install pre-commit hooks."""
-    c.run(f"{VENV_BIN}/pre-commit install", pty=PTY)
+    c.run(f"{poetry_path} lock -n", pty=local_pty)
+    c.run(f"{poetry_path} install -n", pty=local_pty)
 
 
 # Formatting, linting and other checks
-@task(aliases=["format"])
+@task(call(venv, hide=True), aliases=["format"])
 def codestyle(c: Context, check: bool = False) -> None:
     """Format the entire project with `ruff format`."""
     flag = "--check" if check else ""
 
-    c.run(f"{VENV_BIN}/ruff format {flag}", pty=PTY)
+    c.run(f"{c.venv_bin_path}/ruff format {flag}", pty=IS_UNIX_OS)
 
 
-@task
-def check_linter(c: Context, fix: bool = False) -> None:
+@task(call(venv, hide=True), aliases=["check-linter"])
+def lint(c: Context, fix: bool = False) -> None:
     """Check linting rules with `ruff check`."""
     flag = "--fix" if fix else ""
 
-    c.run(f"{VENV_BIN}/ruff check {flag}", pty=PTY)
+    c.run(f"{c.venv_bin_path}/ruff check {flag}", pty=IS_UNIX_OS)
 
 
-@task
+@task(call(venv, hide=True))
 def test(c: Context) -> None:
-    """Run tests with `pytest` and `pyproject.toml` configuration."""
-    c.run(f"{VENV_BIN}/pytest -c pyproject.toml tests", pty=PTY)
+    """Run tests with Pytest and `pyproject.toml` configuration."""
+    c.run(f"{c.venv_bin_path}/pytest -c pyproject.toml tests", pty=IS_UNIX_OS)
 
 
-@task
+@task(call(venv, hide=True))
 def coverage(c: Context) -> None:
     """Generate coverage file in XML for integration with Codacy."""
-    c.run(f"{VENV_BIN}/coverage xml", pty=PTY)
+    c.run(f"{c.venv_bin_path}/coverage xml", pty=IS_UNIX_OS)
 
 
-@task
-def mypy(c: Context) -> None:
-    """Run type checks with `mypy` and `pyproject.toml` configuration."""
-    c.run(f"{VENV_BIN}/mypy --config-file pyproject.toml", pty=PTY)
+@task(call(venv, hide=True), pyproject, aliases=["safety", "check-safety", "sec"])
+def security(c: Context) -> None:
+    """Perform security checks with Bandit."""
+    c.run(
+        f"{c.venv_bin_path}/bandit -ll -c pyproject.toml --recursive hooks",
+        pty=IS_UNIX_OS,
+    )
 
 
-@task(poetry_check)
-def check_safety(c: Context) -> None:
-    """Perform security checks with Safety CLI and `bandit`."""
-    c.run(f"{VENV_BIN}/safety check --full-report --ignore 70612", pty=PTY)
-    c.run(f"{VENV_BIN}/bandit -ll --recursive hooks", pty=PTY)
-
-
-@task
+@task(call(venv, hide=True))
 def sweep(c: Context) -> None:
     """Perform all code checks, including tests, linting and security."""
     test(c)
-    coverage(c)
-    check_linter(c)
-    codestyle(c)
+    lint(c)
+    codestyle(c, check=True)
     mypy(c)
-    check_safety(c)
+    security(c)
 
 
 # Cleaning commands for Bash, Zsh and PowerShell
-@task(aliases=["pycache-clean"])
-def pycache_remove(c: Context) -> None:
+@task(aliases=["rm-cache", "clean-cache"])
+def remove_cache(c: Context) -> None:
     """Remove pycache files from project directory."""
-    c.run(FILE_REMOVER.format(r"(__pycache__|\.pyc|\.pyo)$"))
+    c.run(FILE_REMOVER.format(r"(__pycache__|\.pyc|\.pyo)$"), pty=IS_UNIX_OS)
 
 
-@task(aliases=["dsstore-clean"])
-def dsstore_remove(c: Context) -> None:
+@task(aliases=["rm-dsstore", "clean-dsstore"])
+def remove_dsstore(c: Context) -> None:
     """Remove DS Store files from project directory."""
-    c.run(FILE_REMOVER.format(".DS_Store"))
+    c.run(FILE_REMOVER.format(".DS_Store"), pty=IS_UNIX_OS)
 
 
-@task(aliases=["mypycache-clean", "mypy-remove", "mypy-clean"])
-def mypycache_remove(c: Context) -> None:
+@task(aliases=["rm-mypy", "clean-mypy"])
+def remove_mypy(c: Context) -> None:
     """Remove mypy cache files from project directory."""
-    c.run(FILE_REMOVER.format(".mypy_cache"))
+    c.run(FILE_REMOVER.format(".mypy_cache"), pty=IS_UNIX_OS)
 
 
-@task(aliases=["ipynbcheckpoints-clean", "ipynb-clean", "ipynb-remove"])
-def ipynbcheckpoints_remove(c: Context) -> None:
+@task(aliases=["rm-ipynb", "clean-ipynb"])
+def remove_ipynb(c: Context) -> None:
     """Remove ipynb checkpoints from project directory."""
-    c.run(FILE_REMOVER.format(".ipynb_checkpoints"))
+    c.run(FILE_REMOVER.format(".ipynb_checkpoints"), pty=IS_UNIX_OS)
 
 
-@task(aliases=["pytestcache-clean", "pytest-remove", "pytest-clean"])
-def pytestcache_remove(c: Context) -> None:
+@task(aliases=["rm-pytest", "clean-pytest"])
+def remove_pytest(c: Context) -> None:
     """Remove Pytest cache files from project directory."""
-    c.run(FILE_REMOVER.format(r"(.pytest_cache|.coverage)"))
+    c.run(
+        FILE_REMOVER.format(r"(.pytest_cache|.coverage|test_report.xml)"),
+        pty=IS_UNIX_OS,
+    )
 
 
-@task(aliases=["ruffcache-clean", "ruff-remove", "ruff-clean"])
-def ruffcache_remove(c: Context) -> None:
+@task(aliases=["rm-ruff", "clean-ruff"])
+def remove_ruff(c: Context) -> None:
     """Remove Ruff cache files from project directory."""
-    c.run(FILE_REMOVER.format(".ruff_cache"))
+    c.run(FILE_REMOVER.format(".ruff_cache"), pty=IS_UNIX_OS)
 
 
 @task
 def cleanup(c: Context) -> None:
-    """Perform all cleaning-related tasks."""
-    pycache_remove(c)
-    dsstore_remove(c)
-    mypycache_remove(c)
-    ipynbcheckpoints_remove(c)
-    pytestcache_remove(c)
-    ruffcache_remove(c)
+    """Perform all cleaning-related tasks.
+
+    Does not remove the `build` directory. This should be removed using
+    `invoke remove-build`.
+    """
+    remove_cache(c)
+    remove_dsstore(c)
+    remove_mypy(c)
+    remove_ipynb(c)
+    remove_pytest(c)
+    remove_ruff(c)
 
 
-@task
-def build_remove(c: Context) -> None:
+@task(aliases=["rm-build", "clean-build"])
+def remove_build(c: Context) -> None:
     """Remove the `build` directory."""
-    c.run("rm -rf build/")
+    c.run("rm -rf build/", pty=IS_UNIX_OS)
--- utils/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Module for utilites for project development."""
