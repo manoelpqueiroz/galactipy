a60c0d23fc746b86ed741a447a28f680ed684c2d
Manoel Pereira de Queiroz <mpq.dev@pm.me>
2025-09-23 17:07:33 -0300

    :sparkles: Implement config manager functionality for template
    Created a `config` package to automatically handle the creation and
    management of configuration files, separating base settings from
    secrets using Dynamanager.
    
    Additionally, since users will be able to provide custom values for
    their configuration files, two class methods have been included to
    directly return the default expected configuration or a custom one
    without having to instatiate the whole class.
    
    Changelog: feature

---
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/config/__init__.py
@@ -0,0 +1,3 @@
+from {{ cookiecutter.package_name }}.config.manager import AppManager
+
+__all__ = ["AppManager"]
---
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/config/manager.py
@@ -0,0 +1,46 @@
+"""Configuration manager for {{ cookiecutter.project_name }}."""
+from enum import Enum
+from pathlib import Path
+
+from dynamanager import Dynamanager
+
+from {{ cookiecutter.package_name }}.config.constants import (
+    generate_default_config_schema, get_default_config
+)
+
+
+class EnvKeys(Enum):
+    SETTINGS = "{{ cookiecutter.__envvar }}"
+    SECRETS = "{{ cookiecutter.__envvar }}_SECRET"
+
+
+class AppManager:
+    def __init__(self):
+        self._manager = Dynamanager(
+            default_contents=generate_default_config_schema(),
+            base_dir=get_default_config(),
+        )
+
+    @classmethod
+    def default(cls):
+        instance = cls()._manager
+        instance.add("settings", envvar_prefix=EnvKeys.SETTINGS.value)
+        instance.add("secrets", envvar_prefix=EnvKeys.SECRETS.value)
+
+        return instance
+
+    @classmethod
+    def custom(cls, path: Path, is_secret: bool):
+        instance = cls()._manager
+
+        if is_secret:
+            instance.add(
+                "secrets", custom_config=path, envvar_prefix=EnvKeys.SECRETS.value
+            )
+
+        else:
+            instance.add(
+                "settings", custom_config=path, envvar_prefix=EnvKeys.SETTINGS.value
+            )
+
+        return instance
