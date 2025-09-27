a97a3e3682acc88711884ce3b05cca20847a7b3b
Manoel Pereira de Queiroz <mpq.dev@pm.me>
2025-09-27 13:20:39 -0300

    :art: Improve `AppManager` API
    Modified the `AppManager` class to inherit directly from `Dynamanager`,
    enabling more direct operations with the settings without having to
    always access an isolated class' attribute.
    
    Closes #312
    
    Changelog: enhancement

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/config/manager.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/config/manager.py
@@ -1,11 +1,13 @@
 """Configuration manager for {{ cookiecutter.project_name }}."""
+
 from enum import Enum
 from pathlib import Path
 
 from dynamanager import Dynamanager
 
 from {{ cookiecutter.package_name }}.config.constants import (
-    generate_default_config_schema, get_default_config
+    generate_default_config_schema,
+    get_default_config,
 )
 
 
@@ -14,16 +16,16 @@ class EnvKeys(Enum):
     SECRETS = "{{ cookiecutter.__envvar }}_SECRET"
 
 
-class AppManager:
+class AppManager(Dynamanager):
     def __init__(self):
-        self._manager = Dynamanager(
+        super().__init__(
             default_contents=generate_default_config_schema(),
             base_dir=get_default_config(),
         )
 
     @classmethod
     def default(cls):
-        instance = cls()._manager
+        instance = cls()
         instance.add("settings", envvar_prefix=EnvKeys.SETTINGS.value)
         instance.add("secrets", envvar_prefix=EnvKeys.SECRETS.value)
 
@@ -31,7 +33,7 @@ class AppManager:
 
     @classmethod
     def custom(cls, path: Path, is_secret: bool):
-        instance = cls()._manager
+        instance = cls()
 
         if is_secret:
             instance.add(
