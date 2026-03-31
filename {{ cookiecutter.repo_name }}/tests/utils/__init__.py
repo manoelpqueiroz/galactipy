564ebf2df88de671653ed359a2b247c9b802d1fb
Manoel Pereira de Queiroz <mpq.dev@pm.me>
2026-03-31 18:25:04 -0300

    :pencil: Add docstrings for `tests.utils` objects
    The mkdocstrings library requires objects to be documented in order to
    be collected as members of a higher object. Since the development
    reference will collect test objects by module, these changes were
    required so `boolean_parser` is correctly listed in the reference.
    
    Closes #564
    
    Changelog: docs

--- {{ cookiecutter.repo_name }}/tests/utils/__init__.py
+++ {{ cookiecutter.repo_name }}/tests/utils/__init__.py
@@ -1,8 +1,8 @@
 from tests.utils.parsers import boolean_parser
 {%- if cookiecutter.__app_group == 'tui' %}
-from tests.utils.pytest_bdd_async import async_step
+from tests.utils.pytest_bdd_async import AsyncStepConverter, async_step
 
-__all__ = ["async_step", "boolean_parser"]
+__all__ = ["AsyncStepConverter", "async_step", "boolean_parser"]
 {%- else %}
 
 __all__ = ["boolean_parser"]
