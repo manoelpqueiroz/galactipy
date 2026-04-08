e3dc855e0410ff96164993cb65284130e882f3b6
Manoel Pereira de Queiroz <mpq.dev@pm.me>
2026-04-08 12:02:21 -0300

    :truck: Turn log parsers into public API objects
    Since `_internal` package objects should not be accessed by external
    packages, the `parsers` module was moved to become a public module of
    the `logging` package, enabling users to import its objects directly
    from it.
    
    Changelog: enhancement

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/logging/__init__.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/logging/__init__.py
@@ -1,5 +1,6 @@
 """Define logging capabilities for {{ cookiecutter.project_name }}."""
 
 from {{ cookiecutter.package_name }}.logging.app_logger import setup_app_logging
+from {{ cookiecutter.package_name }}.logging.parsers import FILE_PARSER, log_caster
 
-__all__ = ["setup_app_logging"]
+__all__ = ["FILE_PARSER", "log_caster", "setup_app_logging"]
--- {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/logging/_internal/__init__.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/logging/_internal/__init__.py
@@ -1,5 +1,4 @@
 from {{ cookiecutter.package_name }}.logging._internal.formatters import file_formatter
-from {{ cookiecutter.package_name }}.logging._internal.parsers import log_caster
 from {{ cookiecutter.package_name }}.logging._internal.text_tools import LoggerFormatter
 
-__all__ = ["LoggerFormatter", "file_formatter", "log_caster"]
+__all__ = ["LoggerFormatter", "file_formatter"]
