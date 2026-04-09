e096ba93e2c64deff8fc67b4988fa416a95de088
Manoel Pereira de Queiroz <mpq.dev@pm.me>
2026-04-09 18:49:43 -0300

    :art: Keep internal objects isolated in their packages
    While the Ruff rule PLC2701 is not raised with `get_default_log_path`
    being imported to another package outside `config`, it is best practice
    to keep it internal to the `logging` module.
    
    Changelog: enhancement

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/config/_internal/__init__.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/config/_internal/__init__.py
@@ -1,11 +1,9 @@
 from {{ cookiecutter.package_name }}.config._internal.constants import (
     generate_default_config_schema,
     get_default_config,
-    get_default_log_path,
 )
 
 __all__ = [
     "generate_default_config_schema",
     "get_default_config",
-    "get_default_log_path",
 ]
--- {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/logging/_internal/__init__.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/logging/_internal/__init__.py
@@ -1,4 +1,5 @@
+from {{ cookiecutter.package_name }}.logging._internal.constants import get_default_log_path
 from {{ cookiecutter.package_name }}.logging._internal.formatters import file_formatter
 from {{ cookiecutter.package_name }}.logging._internal.text_tools import LoggerFormatter
 
-__all__ = ["LoggerFormatter", "file_formatter"]
+__all__ = ["LoggerFormatter", "file_formatter", "get_default_log_path"]
