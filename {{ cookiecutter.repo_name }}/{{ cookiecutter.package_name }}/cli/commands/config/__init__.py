f49a0571dd680ed360052b4d92715616a1b7d410
Manoel Pereira de Queiroz <mpq.dev@pm.me>
2026-04-20 20:44:31 -0300

    :rotating_light: Fix Ruff warnings for restructured template
    After altering the template's project structure, with file and function
    additions, the template went untested, thus requiring an ad hoc fix.
    
    Changelog: fixes

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/cli/commands/config/__init__.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/cli/commands/config/__init__.py
@@ -8,10 +8,7 @@ from {{ cookiecutter.package_name }}.cli.commands.config.set import config_set_a
 from {{ cookiecutter.package_name }}.cli.commands.config.unset import config_unset_app
 from {{ cookiecutter.package_name }}.cli.helpers import naked_command
 
-config_app = typer.Typer(
-    rich_markup_mode="rich",
-    help=":gear: Perform operations with {{ cookiecutter.project_name }} configuration.",
-)
+config_app = typer.Typer(rich_markup_mode="rich")
 
 config_app.add_typer(config_get_app)
 config_app.add_typer(config_set_app)
@@ -20,5 +17,6 @@ config_app.add_typer(config_extend_app)
 
 
 @naked_command(config_app)
-def main(ctx: typer.Context):
+def main(ctx: typer.Context) -> None:
+    """:gear: Perform operations with {{ cookiecutter.project_name }} configuration."""
     pass
